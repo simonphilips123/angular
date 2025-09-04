835129d5a7e381c75b658d45542baa53dcbcaafd
Simon <itssimon.cse@gmail.com>
2025-09-04 13:15:11 +0530

    initial commit

---
+++ angular/script.js
@@ -0,0 +1,9 @@
+var n=5
+var s="hello"
+var bol=true
+
+document.getElementById('box').innerHTML=n+5
+
+document.getElementById('box').addEventListener('click',function(){
+    alert("clicked here")
+}) 
